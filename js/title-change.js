05d44a1062f05894bffedba30997e5c861556491
wpz <wang.peizhang@ikasinfo.com>
2024-06-22 21:38:19 +0800

    Site updated: 2024-06-22 21:38:18

--- js/title-change.js
+++
@@ -1,1 +0,0 @@
-var st,originTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="(๑•́ ₃ •̀๑) 哎呀走丢啦！",clearTimeout(st)):(document.title="o(≧v≦)o 嘻又回来啦！",st=setTimeout(function(){document.title=originTitile},4e3))});
